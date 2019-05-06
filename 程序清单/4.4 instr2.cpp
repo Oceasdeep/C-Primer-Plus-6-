3364cdd5487cda947b76c108b3c943eb83d647e3
Oceansdeep <35598834+Oceasdeep@users.noreply.github.com>
2019-05-06 21:45:18 +0800

    Create 4.4 instr2.cpp

---
+++ 程序清单/4.4 instr2.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name: \n";
+    cin.getline(name,ArSize);
+    cout << "Enter your favorite dessert:\n";
+    cin.getline(dessert,ArSize);
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name <<". \n";
+    return 0;
+}
+
+运行结果：
+
+  Enter your name: 
+  xu hai
+  Enter your favorite dessert:
+  fish eggs
+  I have some delicious fish eggs for you, xu hai. 
