b34a46f4e6c384a2bedef98b9f7b37f527155181
Oceansdeep <35598834+Oceasdeep@users.noreply.github.com>
2019-05-06 22:06:49 +0800

    cin.get(name,ArSize).get()
    超过19个字符不再读入

---
+++ 程序清单/4.5 instr3.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    const int ArSize = 20;
+    char name[ArSize];
+    char dessert[ArSize];
+
+    cout << "Enter your name: \n";
+    cin.get(name,ArSize).get();
+    cout << "Enter your favorite dessert:\n";
+    cin.get(dessert,ArSize).get();
+    cout << "I have some delicious " << dessert;
+    cout << " for you, " << name <<". \n";
+    return 0;
+}
+
+
+运行结果：
+
+Enter your name: 
+oceans deep
+Enter your favorite dessert:
+noodles with fish balls
+I have some delicious noodles with fish b for you, oceans deep.
